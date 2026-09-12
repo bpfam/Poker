b5468970364e607f11388aa92e5970fe504356d4
bpfam <joker1312boos@icloud.com>
2026-09-12 15:59:04 +0200

    Fix formatting of SUPABASE_URL export

--- config.js
+++ config.js
@@ -8,5 +8,5 @@
 // NON inserire mai la service_role key nel sito.
 
 export const DEMO_MODE = false;
-export const SUPABASE_URL = 'https://vigaczxnenshvtbcgkcd.supabase.co ;
+export const SUPABASE_URL = 'https://vigaczxnenshvtbcgkcd.supabase.co;
   export const SUPABASE_ANON_KEY = 'sb_publishable_RTwd1nk40tut0VeQ1LNUQw_JawNOh1j;
