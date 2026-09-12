903081ac1c124e500e67d4b4816c329c3c6c064b
bpfam <joker1312boos@icloud.com>
2026-09-12 16:15:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+// POKER - CONFIGURAZIONE SUPABASE
+
 export const DEMO_MODE = false;
+
 export const SUPABASE_URL = 'https://vigaczxnenshvtbcgkcd.supabase.co';
-export const SUPABASE_ANON_KEY = 'LA_TUA_PUBLISHABLE_KEY_COMPLETA';
+
+export const SUPABASE_ANON_KEY = 'sb_publishable_RTwd1nk40tut0VeQ1LNUQw_JawNOh1j';
