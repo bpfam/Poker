6a5bf63378b2c6a83b0cd9cdc5f1a70d6a430bbf
bpfam <joker1312boos@icloud.com>
2026-09-12 15:58:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
 // 2. Inserisci URL e anon key del tuo progetto Supabase.
 // NON inserire mai la service_role key nel sito.
 
-export const DEMO_MODE = true;
-export const SUPABASE_URL = 'https://TUO-PROGETTO.supabase.co';
-export const SUPABASE_ANON_KEY = 'INSERISCI-ANON-KEY';
+export const DEMO_MODE = false;
+export const SUPABASE_URL = 'https://vigaczxnenshvtbcgkcd.supabase.co ;
+  export const SUPABASE_ANON_KEY = 'sb_publishable_RTwd1nk40tut0VeQ1LNUQw_JawNOh1j;
